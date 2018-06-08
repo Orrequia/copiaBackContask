cc8f091ca112ad0f835c070c2945c5ece7cdacd6
Orrequia <orrequia8@gmail.com>
2018-06-08 19:00:41 +0200

    Relacion con Store

--- src/main/java/com/fot/backConTask/model/Company.java
+++ src/main/java/com/fot/backConTask/model/Company.java
@@ -1,14 +1,27 @@
 package com.fot.backConTask.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 @Entity
 public class Company {
 
+	public static final String FIELD_IDCOMPANY = "idCompany";
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer idCompany;
@@ -21,4 +34,8 @@ public class Company {
 	
 	@Column(nullable=true)
 	private String note;
+	
+	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.REMOVE)
+	@JoinColumn(name=FIELD_IDCOMPANY, referencedColumnName=FIELD_IDCOMPANY)
+	private List<Store> store;
 }
