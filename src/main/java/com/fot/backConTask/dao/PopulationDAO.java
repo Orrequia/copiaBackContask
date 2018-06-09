801aede906cf98eb721c84aee13457c15ba54429
Orrequia <orrequia8@gmail.com>
2018-06-09 10:30:35 +0200

    Creado repository de población

---
+++ src/main/java/com/fot/backConTask/dao/PopulationDAO.java
@@ -0,0 +1,10 @@
+package com.fot.backConTask.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.fot.backConTask.model.Population;
+
+@Repository
+public interface PopulationDAO extends GenericDAO<Population> {
+
+}
