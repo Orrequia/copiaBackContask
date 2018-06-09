b6a724f8195e21f8968f1253fec0d96c078616dc
Orrequia <orrequia8@gmail.com>
2018-06-09 10:32:51 +0200

    Creado repository de la tienda

---
+++ src/main/java/com/fot/backConTask/dao/StoreDAO.java
@@ -0,0 +1,10 @@
+package com.fot.backConTask.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.fot.backConTask.model.Store;
+
+@Repository
+public interface StoreDAO extends GenericDAO<Store> {
+
+}
