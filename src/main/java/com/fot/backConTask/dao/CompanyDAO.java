51fced0020bfd32a2c27e674f01b9f8d1a31ce15
Orrequia <orrequia8@gmail.com>
2018-06-09 10:33:35 +0200

    Creado repository de empresa

---
+++ src/main/java/com/fot/backConTask/dao/CompanyDAO.java
@@ -0,0 +1,10 @@
+package com.fot.backConTask.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.fot.backConTask.model.Company;
+
+@Repository
+public interface CompanyDAO extends GenericDAO<Company> {
+
+}
