4473b82247ec6b0e5aaa669ee5b2a64db053b304
Orrequia <orrequia8@gmail.com>
2018-06-09 10:24:14 +0200

    Creado repository del rol

---
+++ src/main/java/com/fot/backConTask/dao/RoleDAO.java
@@ -0,0 +1,10 @@
+package com.fot.backConTask.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.fot.backConTask.model.Role;
+
+@Repository
+public interface RoleDAO extends GenericDAO<Role>{
+
+}
