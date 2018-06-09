e68fd482c0ecacff7a546b623c66601c88e450ac
Orrequia <orrequia8@gmail.com>
2018-06-09 08:37:03 +0200

    Creada entidad provincia y su relación con población

---
+++ src/main/java/com/fot/backConTask/model/Province.java
@@ -0,0 +1,34 @@
+package com.fot.backConTask.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Province {
+	
+	private static final String FIELD_IDPROVINCE = "idProvince";
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer idProvince;
+	
+	@Column(nullable=false)
+	private String name;
+	
+	@OneToMany(fetch=FetchType.LAZY)
+	@JoinColumn(name=FIELD_IDPROVINCE, referencedColumnName=FIELD_IDPROVINCE)
+	private List<Population> population;
+}
