4ed0e5cbc00f6392e593a958c74abde532e41db2
Orrequia <orrequia8@gmail.com>
2018-06-09 10:26:07 +0200

    Creado repository del tipo de mochila

---
+++ src/main/java/com/fot/backConTask/dao/PenpackTypeDAO.java
@@ -0,0 +1,10 @@
+package com.fot.backConTask.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.fot.backConTask.model.PenpackType;
+
+@Repository
+public interface PenpackTypeDAO extends GenericDAO<PenpackType>{
+
+}
