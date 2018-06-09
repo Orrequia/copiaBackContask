0490cdccfb4aa619e11833f6cd2783f84e2e8be6
Orrequia <orrequia8@gmail.com>
2018-06-09 10:25:00 +0200

    Creado repository de privilegio

---
+++ src/main/java/com/fot/backConTask/dao/PrivilegeDAO.java
@@ -0,0 +1,10 @@
+package com.fot.backConTask.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.fot.backConTask.model.Privilege;
+
+@Repository
+public interface PrivilegeDAO extends GenericDAO<Privilege> {
+
+}
