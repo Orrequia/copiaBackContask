a7ca481ebacfa571b513ab67d35862c1326c5f07
Orrequia <orrequia8@gmail.com>
2018-06-09 09:02:33 +0200

    Creada entidad tipo de contrato

---
+++ src/main/java/com/fot/backConTask/model/ContractType.java
@@ -0,0 +1,23 @@
+package com.fot.backConTask.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class ContractType {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer idContractType;
+	
+	@Column(nullable=false, unique=true)
+	private String name;
+}
