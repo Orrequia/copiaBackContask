76d426e4810e41f3aeaf95cef9ba82057c177ce1
Orrequia <orrequia8@gmail.com>
2018-06-09 10:29:07 +0200

    Creado repository de contrato

---
+++ src/main/java/com/fot/backConTask/dao/ContractDAO.java
@@ -0,0 +1,10 @@
+package com.fot.backConTask.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.fot.backConTask.model.Contract;
+
+@Repository
+public interface ContractDAO extends GenericDAO<Contract>{
+
+}
