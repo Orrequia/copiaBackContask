26d22ee3be0e7c5cb47e70d40b7741cf12b0a919
Orrequia <orrequia8@gmail.com>
2018-06-09 10:29:48 +0200

    Creado repository de provincia

---
+++ src/main/java/com/fot/backConTask/dao/ProvinceDAO.java
@@ -0,0 +1,10 @@
+package com.fot.backConTask.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.fot.backConTask.model.Province;
+
+@Repository
+public interface ProvinceDAO extends GenericDAO<Province>{
+
+}
