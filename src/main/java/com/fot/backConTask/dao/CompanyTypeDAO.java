9e3efab4c847f630bf454aaf93ba3fe464b170c6
Orrequia <orrequia8@gmail.com>
2018-06-09 10:31:24 +0200

    Creado repository del tipo de empresa

---
+++ src/main/java/com/fot/backConTask/dao/CompanyTypeDAO.java
@@ -0,0 +1,10 @@
+package com.fot.backConTask.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.fot.backConTask.model.CompanyType;
+
+@Repository
+public interface CompanyTypeDAO extends GenericDAO<CompanyType>{
+
+}
