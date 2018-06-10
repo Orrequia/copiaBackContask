4c2fef9747fb94821a4dee888a0af2cf0d22a36c
Orrequia <orrequia8@gmail.com>
2018-06-10 13:38:36 +0200

    Añadido posible excepción comprobando los parámetros incorrectos.

--- src/main/java/com/fot/backConTask/service/Service.java
+++ src/main/java/com/fot/backConTask/service/Service.java
@@ -6,11 +6,13 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Pageable;
 
+import com.fot.backConTask.exception.IncorrectParametersException;
+
 public interface Service<T, ID extends Serializable> {
 
 	T create(T t);
 	void update(T t);
 	Optional<T> findById(ID id);
-	List<T> findAll(Pageable p);
+	List<T> findAll(Pageable p) throws IncorrectParametersException;
 	void delete(T t);
 }
