43dcb5c2cfeaacc42246fdb558446096ed5dbe58
Orrequia <orrequia8@gmail.com>
2018-06-10 13:34:02 +0200

    Interfaz de servicio abstracto

---
+++ src/main/java/com/fot/backConTask/service/Service.java
@@ -0,0 +1,16 @@
+package com.fot.backConTask.service;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Pageable;
+
+public interface Service<T, ID extends Serializable> {
+
+	T create(T t);
+	void update(T t);
+	Optional<T> findById(ID id);
+	List<T> findAll(Pageable p);
+	void delete(T t);
+}
