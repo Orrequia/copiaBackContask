547e77afbdcf67e8636c6ffcbd44ffb4e2a597e3
Orrequia <orrequia8@gmail.com>
2018-06-22 18:12:34 +0200

    añadido método para buscar un usuario por username y contraseña

--- src/main/java/com/fot/backConTask/dao/UserDAO.java
+++ src/main/java/com/fot/backConTask/dao/UserDAO.java
@@ -1,5 +1,7 @@
 package com.fot.backConTask.dao;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
 
 import com.fot.backConTask.model.User;
@@ -7,4 +9,5 @@ import com.fot.backConTask.model.User;
 @Repository
 public interface UserDAO extends GenericDAO<User> {
 
+	public Optional<User> findOneByUsernameAndPassword(String username, String password);
 }
