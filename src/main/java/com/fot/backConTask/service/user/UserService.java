1228b492c6983d2d237844a6a7f60d03dff6b15a
Orrequia <orrequia8@gmail.com>
2018-06-22 18:47:53 +0200

    Implementado método para obtener un usuario por su username y password

--- src/main/java/com/fot/backConTask/service/user/UserService.java
+++ src/main/java/com/fot/backConTask/service/user/UserService.java
@@ -1,8 +1,11 @@
 package com.fot.backConTask.service.user;
 
+import com.fot.backConTask.dto.LoginDTO;
+import com.fot.backConTask.exception.InvalidUserException;
 import com.fot.backConTask.model.User;
 import com.fot.backConTask.service.Service;
 
 public interface UserService extends Service<User, Integer> {
-
+	
+	public User findUser(LoginDTO login) throws InvalidUserException;
 }
