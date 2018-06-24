5ff396af68cff70538e4e94fe877da2599e79f87
Orrequia <orrequia8@gmail.com>
2018-06-24 22:03:17 +0200

    Excepción creada para un no encontrado

---
+++ src/main/java/com/fot/backcontask/exception/NotFoundException.java
@@ -0,0 +1,16 @@
+package com.fot.backcontask.exception;
+
+public class NotFoundException extends Exception {
+
+	private static final long serialVersionUID = 1689769876L;
+
+	public static final String MSG = "Esta entiedad no existe";
+	
+	public NotFoundException(String message) {
+		super(message);
+	}
+	
+	public NotFoundException() {
+		super(MSG);
+}
+}
