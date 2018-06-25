1ecd3ab31cc3be9be04a28f8ee9c87b1fa1199bc
Orrequia <orrequia8@gmail.com>
2018-06-25 19:14:17 +0200

    Dto para el usuario

---
+++ src/main/java/com/fot/backcontask/dto/user/UserDTO.java
@@ -0,0 +1,11 @@
+package com.fot.backcontask.dto.user;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+	private Long idUser;
+	private String name;
+	private String email;
+	private String phone;
+}
