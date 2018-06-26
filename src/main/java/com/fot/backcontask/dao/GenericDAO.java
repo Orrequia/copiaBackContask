cd3a49b07866a31fa491b6a3404f7ee867014561
Orrequia <orrequia8@gmail.com>
2018-06-26 20:09:37 +0200

    Cambiado Integer por Long

--- src/main/java/com/fot/backcontask/dao/GenericDAO.java
+++ src/main/java/com/fot/backcontask/dao/GenericDAO.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 @NoRepositoryBean
-public interface GenericDAO<D> extends PagingAndSortingRepository<D, Integer> {
+public interface GenericDAO<D> extends PagingAndSortingRepository<D, Long> {
 
 }
