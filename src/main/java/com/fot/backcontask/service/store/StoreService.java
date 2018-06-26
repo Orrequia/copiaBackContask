0262c7561e79ed77cb6f84e071bf4d059b5d2963
Orrequia <orrequia8@gmail.com>
2018-06-26 20:13:14 +0200

    Cambiado Integer por Long para los id's

--- src/main/java/com/fot/backcontask/service/store/StoreService.java
+++ src/main/java/com/fot/backcontask/service/store/StoreService.java
@@ -3,6 +3,6 @@ package com.fot.backcontask.service.store;
 import com.fot.backcontask.model.Store;
 import com.fot.backcontask.service.Service;
 
-public interface StoreService extends Service<Store, Integer> {
+public interface StoreService extends Service<Store, Long> {
 
 }
