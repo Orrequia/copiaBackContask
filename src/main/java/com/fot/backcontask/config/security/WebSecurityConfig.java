309c147c3f96d466cf5320e9460c32eccda359e1
forrequia <forrequia@AT-GSP5RN2.atsistemas.lan>
2018-07-06 14:46:29 +0200

    Añadida anotaciones correspondientes.

--- src/main/java/com/fot/backcontask/config/security/WebSecurityConfig.java
+++ src/main/java/com/fot/backcontask/config/security/WebSecurityConfig.java
@@ -4,15 +4,20 @@ import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Resource(name = "userService")
@@ -39,12 +44,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(final HttpSecurity http) throws Exception {
 		http
-			.authorizeRequests()
-				.antMatchers("/").permitAll()
-				.antMatchers("/auth/**").permitAll()
-				.antMatchers("/user").permitAll()
-			.and()
 			.anonymous().disable()
+			//.requestMatchers()
+			//	.antMatchers("/oauth/authorize").and()
+			.authorizeRequests()
+				//.antMatchers("/").permitAll()
+				//.antMatchers("/oauth/authorize").permitAll()//.hasRole("ADMIN")
+				.antMatchers("/user").permitAll()
+			//	.anyRequest().authenticated()
+			.and()
 			.csrf().disable()
 			.cors();			
 	}
