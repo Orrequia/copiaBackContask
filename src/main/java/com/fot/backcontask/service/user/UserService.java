f21d71842859a26356f63eb1ca9d7ff638df1317
forrequia <forrequia@AT-GSP5RN2.atsistemas.lan>
2018-07-06 13:17:18 +0200

    Subido métodos a la interfaz superior de Servicio, común en todos.

--- src/main/java/com/fot/backcontask/service/user/UserService.java
+++ src/main/java/com/fot/backcontask/service/user/UserService.java
@@ -1,12 +1,8 @@
 package com.fot.backcontask.service.user;
 
-import com.fot.backcontask.exception.NotFoundException;
 import com.fot.backcontask.model.User;
 import com.fot.backcontask.service.Service;
 
 public interface UserService extends Service<User, Long> {
 	
-	User getAndCheck(Long idUser) throws NotFoundException;
-	boolean isEqual(User u1, User u2);
-	void setValues(User to, User from);
 }
