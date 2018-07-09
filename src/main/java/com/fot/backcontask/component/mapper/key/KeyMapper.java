f626ad77390b7ce636d07965cebe5f2cebe53b88
forrequia <forrequia@AT-GSP5RN2.atsistemas.lan>
2018-07-09 12:17:19 +0200

    Corregido import erróneo y transformado de KeyType a Long

--- src/main/java/com/fot/backcontask/component/mapper/key/KeyMapper.java
+++ src/main/java/com/fot/backcontask/component/mapper/key/KeyMapper.java
@@ -1,8 +1,8 @@
 package com.fot.backcontask.component.mapper.key;
 
-import com.fasterxml.classmate.util.ResolvedTypeCache.Key;
 import com.fot.backcontask.component.mapper.Mapper;
 import com.fot.backcontask.dto.key.KeyDTO;
+import com.fot.backcontask.model.Key;
 
 public interface KeyMapper extends Mapper<Key, KeyDTO> {
 }
--- src/main/java/com/fot/backcontask/component/mapper/key/KeyMapperImpl.java
+++ src/main/java/com/fot/backcontask/component/mapper/key/KeyMapperImpl.java
@@ -1,11 +1,18 @@
 package com.fot.backcontask.component.mapper.key;
 
-import com.fasterxml.classmate.util.ResolvedTypeCache.Key;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.fot.backcontask.component.mapper.AbstractMapper;
 import com.fot.backcontask.dto.key.KeyDTO;
+import com.fot.backcontask.exception.NotFoundException;
+import com.fot.backcontask.model.Key;
+import com.fot.backcontask.service.keytype.KeyTypeService;
 
 public class KeyMapperImpl extends AbstractMapper<Key, KeyDTO> implements KeyMapper {
 
+	@Autowired
+	KeyTypeService keyTypeService;
+	
 	@Override
 	public Class<? extends KeyDTO> dtoClazz() {
 		return KeyDTO.class;
@@ -16,4 +23,26 @@ public class KeyMapperImpl extends AbstractMapper<Key, KeyDTO> implements KeyMap
 		return Key.class;
 	}
 
+	@Override
+	public Key dtoToModel(KeyDTO dto) throws NotFoundException {
+		return map(dto);
+	}
+	
+	private Key map(KeyDTO dto) throws NotFoundException {
+		Key key = dozer.map(dto, modelClazz());
+		if(dto.getIdKeyType() != null) key.setKeyType(keyTypeService.getAndCheck(dto.getIdKeyType()));
+		return key;
+	}
+
+	@Override
+	public KeyDTO modelToDto(Key model) {
+		return map(model);
+	}
+	
+	private KeyDTO map(Key model) {
+		KeyDTO keyDTO = dozer.map(model, dtoClazz());
+		keyDTO.setIdKeyType(model.getKeyType().getIdKeyType());
+		return keyDTO;
+	}
+	
 }
