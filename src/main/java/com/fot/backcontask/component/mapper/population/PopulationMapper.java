8a3a6c85d2ec006a90a97b127e3241f0b819fb4a
Orrequia <orrequia8@gmail.com>
2018-07-09 19:42:37 +0200

    Mappers de Población

---
+++ src/main/java/com/fot/backcontask/component/mapper/population/PopulationMapper.java
@@ -0,0 +1,9 @@
+package com.fot.backcontask.component.mapper.population;
+
+import com.fot.backcontask.component.mapper.Mapper;
+import com.fot.backcontask.dto.population.PopulationDTO;
+import com.fot.backcontask.model.Population;
+
+public interface PopulationMapper extends Mapper<Population, PopulationDTO> {
+
+}
---
+++ src/main/java/com/fot/backcontask/component/mapper/population/PopulationMapperImpl.java
@@ -0,0 +1,18 @@
+package com.fot.backcontask.component.mapper.population;
+
+import com.fot.backcontask.component.mapper.AbstractMapper;
+import com.fot.backcontask.dto.population.PopulationDTO;
+import com.fot.backcontask.model.Population;
+
+public class PopulationMapperImpl extends AbstractMapper<Population, PopulationDTO> implements PopulationMapper {
+
+	@Override
+	public Class<? extends PopulationDTO> dtoClazz() {
+		return PopulationDTO.class;
+	}
+
+	@Override
+	public Class<? extends Population> modelClazz() {
+		return Population.class;
+	}
+}
