799062e2f7b7cd00e02d5d641489bd854e275e7d
Orrequia <orrequia8@gmail.com>
2018-07-09 19:44:48 +0200

    Controlador de población

---
+++ src/main/java/com/fot/backcontask/controller/PopulationController.java
@@ -0,0 +1,13 @@
+package com.fot.backcontask.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fot.backcontask.dto.population.PopulationDTO;
+import com.fot.backcontask.model.Population;
+
+@RestController
+@RequestMapping("/population")
+public class PopulationController extends AbstractController<Population, PopulationDTO>{
+
+}
