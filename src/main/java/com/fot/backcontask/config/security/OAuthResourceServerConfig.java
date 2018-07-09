1a579833fc798a40264d2fc1206e0cbb8ff3a4f3
forrequia <forrequia@AT-GSP5RN2.atsistemas.lan>
2018-07-09 11:05:47 +0200

    Eliminado comentarios

--- src/main/java/com/fot/backcontask/config/security/OAuthResourceServerConfig.java
+++ src/main/java/com/fot/backcontask/config/security/OAuthResourceServerConfig.java
@@ -1,29 +1,42 @@
 package com.fot.backcontask.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableResourceServer
 public class OAuthResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-	private static final String RESOURCE_ID = "resource_id";
-		
-	@Override
-	public void configure(ResourceServerSecurityConfigurer config) throws Exception {
-		config.resourceId(RESOURCE_ID).stateless(false);
-	}
-
-	@Override
-	public void configure(HttpSecurity http) throws Exception {
-		http
-			.anonymous().disable()
-			.authorizeRequests()
-				.antMatchers("/company/**").access("hasRole('ADMIN')").and()
-			.exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
-	}
+	 @Bean
+	 public TokenStore tokenStore() {
+		 return new JwtTokenStore(accessTokenConverter());
+	 }
+ 
+	 @Bean
+	 public JwtAccessTokenConverter accessTokenConverter() {
+		 JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+		 converter.setSigningKey("123");
+		 return converter;
+	 }
+	    
+	 @Bean
+	 @Primary
+     public DefaultTokenServices tokenServices() {
+         DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+         defaultTokenServices.setTokenStore(tokenStore());
+         return defaultTokenServices;
+     }
+	
+	 @Override
+	 public void configure(ResourceServerSecurityConfigurer config) throws Exception {
+		 config.tokenServices(tokenServices());
+	 }
 }
