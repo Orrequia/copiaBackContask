bee40a7f328088f440d9a2265f8bbe9ba21bf1c3
Orrequia <orrequia8@gmail.com>
2018-07-09 19:43:26 +0200

    Añadida anotación @Component

--- src/main/java/com/fot/backcontask/component/mapper/population/PopulationMapperImpl.java
+++ src/main/java/com/fot/backcontask/component/mapper/population/PopulationMapperImpl.java
@@ -1,9 +1,12 @@
 package com.fot.backcontask.component.mapper.population;
 
+import org.springframework.stereotype.Component;
+
 import com.fot.backcontask.component.mapper.AbstractMapper;
 import com.fot.backcontask.dto.population.PopulationDTO;
 import com.fot.backcontask.model.Population;
 
+@Component
 public class PopulationMapperImpl extends AbstractMapper<Population, PopulationDTO> implements PopulationMapper {
 
 	@Override
