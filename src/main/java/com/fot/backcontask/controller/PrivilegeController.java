8054746d29bdfc07452da0456c4c50e46a2cba3f
Orrequia <orrequia8@gmail.com>
2018-07-09 19:57:58 +0200

    Controlador de privilegios

---
+++ src/main/java/com/fot/backcontask/controller/PrivilegeController.java
@@ -0,0 +1,13 @@
+package com.fot.backcontask.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fot.backcontask.dto.privilege.PrivilegeDTO;
+import com.fot.backcontask.model.Privilege;
+
+@RestController
+@RequestMapping("/privilege")
+public class PrivilegeController extends AbstractController<Privilege, PrivilegeDTO>{
+	
+}
