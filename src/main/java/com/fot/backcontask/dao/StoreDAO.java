d1802866478e1e7f6051697fcf8c9a4db2e80bc2
Orrequia <orrequia8@gmail.com>
2018-07-28 19:52:41 +0200

    Añadida consulta para obtener todas las tiendas de una compañia

--- src/main/java/com/fot/backcontask/dao/StoreDAO.java
+++ src/main/java/com/fot/backcontask/dao/StoreDAO.java
@@ -1,5 +1,10 @@
 package com.fot.backcontask.dao;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.fot.backcontask.model.Store;
@@ -7,4 +12,6 @@ import com.fot.backcontask.model.Store;
 @Repository
 public interface StoreDAO extends GenericDAO<Store> {
 
+	@Query(value = "SELECT s FROM Store AS s JOIN Company AS c WHERE c.idCompany = :idCompany ORDEN BY s.name")
+	List<Store> findByCompany(@Param("idCompany") Long idCompany, Pageable pageable);
 }
