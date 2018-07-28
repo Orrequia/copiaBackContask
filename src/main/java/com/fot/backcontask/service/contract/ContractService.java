944668c8023509bd7001ef0aa2bb2db7519a2ef2
Orrequia <orrequia8@gmail.com>
2018-07-28 20:08:20 +0200

    Añadido métodos para el CRUD de contrato a través de empresa.

--- src/main/java/com/fot/backcontask/service/contract/ContractService.java
+++ src/main/java/com/fot/backcontask/service/contract/ContractService.java
@@ -1,8 +1,19 @@
 package com.fot.backcontask.service.contract;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+
+import com.fot.backcontask.exception.InvalidRequestException;
+import com.fot.backcontask.exception.NotFoundException;
 import com.fot.backcontask.model.Contract;
 import com.fot.backcontask.service.Service;
 
 public interface ContractService extends Service<Contract, Long>{
-
+	
+	Contract findOneContractByCompany(Long idCompany, Long idContract) throws NotFoundException;
+	List<Contract> findContractByCompany(Long idCompany, Pageable p) throws NotFoundException;
+	Contract addToCompany(Long idCompany, Contract contract) throws NotFoundException;
+	void updateToCompany(Long idCompany, Long idContract, Contract newContract) throws NotFoundException;
+	void deleteToCompany(Long idCompany, Long idContract, Contract bodyContract) throws NotFoundException, InvalidRequestException;
 }
--- src/main/java/com/fot/backcontask/service/contract/ContractServiceImpl.java
+++ src/main/java/com/fot/backcontask/service/contract/ContractServiceImpl.java
@@ -1,17 +1,29 @@
 package com.fot.backcontask.service.contract;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.time.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.fot.backcontask.dao.ContractDAO;
+import com.fot.backcontask.exception.InvalidRequestException;
 import com.fot.backcontask.exception.NotFoundException;
+import com.fot.backcontask.model.Company;
 import com.fot.backcontask.model.Contract;
 import com.fot.backcontask.service.AbstractService;
+import com.fot.backcontask.service.company.CompanyService;
 
 @Service
 public class ContractServiceImpl extends AbstractService<Contract, ContractDAO> implements ContractService {
 
+	@Autowired
+	CompanyService companyService;
+	
 	@Override
 	public Contract getAndCheck(Long id) throws NotFoundException {
 		return findById(id).orElseThrow(() -> new NotFoundException("El contrato no existe"));
@@ -33,4 +45,46 @@ public class ContractServiceImpl extends AbstractService<Contract, ContractDAO> 
 		to.setContractLine(from.getContractLine());
 	}
 
+	@Override
+	public Contract findOneContractByCompany(Long idCompany, Long idContract) throws NotFoundException {
+		final Company company = companyService.getAndCheck(idCompany);
+		return getAndCheckBelongCompany(company, idContract);
+	}
+	
+	private Contract getAndCheckBelongCompany(Company company, Long idContract) throws NotFoundException {
+		final Optional<Contract> contract = company.getContract().stream()
+				.filter(q -> q.getIdContract() == idContract)
+				.findFirst();
+		return contract.orElseThrow(() -> new NotFoundException("Este contrato no existe para esta empresa"));
+	}
+	
+	@Override
+	public List<Contract> findContractByCompany(Long idCompany, Pageable p) throws NotFoundException {
+		companyService.getAndCheck(idCompany);
+		return dao.findByCompany(idCompany, PageRequest.of(p.getPageNumber(), p.getPageSize()));
+	}
+	
+	@Override
+	public Contract addToCompany(Long idCompany, Contract contract) throws NotFoundException {
+		final Contract createContract = create(contract);
+		companyService.addContract(companyService.getAndCheck(idCompany), createContract);
+		return createContract;
+	}
+	
+	@Override
+	public void updateToCompany(Long idCompany, Long idContract, Contract newContract) throws NotFoundException {
+		final Company company = companyService.getAndCheck(idCompany);
+		final Contract contract = this.getAndCheckBelongCompany(company, idContract);
+		this.setValues(contract, newContract);
+		this.update(contract);
+	}
+	
+	@Override
+	public void deleteToCompany(Long idCompany, Long idContract, Contract bodyContract) throws NotFoundException, InvalidRequestException {
+		final Company company = companyService.getAndCheck(idCompany);
+		final Contract contract = this.getAndCheckBelongCompany(company, idContract);
+		if(!this.isEqual(bodyContract, contract))
+			throw new InvalidRequestException("El contrato recibido no coincide con el almacenado");
+		companyService.removeContract(company, contract);
+	}
 }
