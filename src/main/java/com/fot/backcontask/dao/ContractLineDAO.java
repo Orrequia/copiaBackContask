413f5471d264535511300c379aad723c80b0c680
Orrequia <orrequia8@gmail.com>
2018-07-29 01:17:56 +0200

    Añadido metodo para buscar líneas de contrato por contrato y empresa

--- src/main/java/com/fot/backcontask/dao/ContractLineDAO.java
+++ src/main/java/com/fot/backcontask/dao/ContractLineDAO.java
@@ -1,10 +1,19 @@
 package com.fot.backcontask.dao;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.fot.backcontask.model.ContractLine;
 
 @Repository
 public interface ContractLineDAO extends GenericDAO<ContractLine>{
-
+	
+	@Query(value = "SELECT conl FROM ContractLine AS conl JOIN Contract AS con JOIN Company AS c "
+			+ "WHERE c.idCompany = :idCompany AND con.idContract = :idContract "
+			+ "ORDEN BY conl.idContractLine")
+	List<ContractLine> findByCompanyAndContract(@Param("idCompany") Long idCompany, @Param("idContract") Long idContract, Pageable pageable);
 }
