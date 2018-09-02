f621ccaaf1bdfdfcd2c97d62176434965d53ed9c
Orrequia <orrequia8@gmail.com>
2018-09-02 10:07:42 +0200

    Desacomplado temas de login con usuario.

---
+++ src/main/java/com/fot/backcontask/controller/LoginController.java
@@ -0,0 +1,73 @@
+package com.fot.backcontask.controller;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+
+import com.fot.backcontask.dto.user.ConnectedDTO;
+import com.fot.backcontask.exception.InvalidRequestException;
+import com.fot.backcontask.exception.NotFoundException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
+public class LoginController {
+
+	private static final Integer LONGTEXTBASIC = 5;
+	
+	@Autowired
+	@Qualifier("authenticationManagerBean")
+	private AuthenticationManager authenticationManager;
+	
+	@PostMapping("/login")
+	public ConnectedDTO login(@RequestHeader("Authorization") String auth) throws NotFoundException, UnsupportedEncodingException, InvalidRequestException {
+		
+	    if (auth != null && auth.startsWith("Basic")) {
+	        String credentials = new String(Base64.getDecoder().decode(auth.substring(LONGTEXTBASIC).trim()), "UTF-8");
+	        final String[] values = credentials.split(":",2);
+	        
+	        if(values.length == 2) { 
+	        	final Authentication token = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(values[0], values[1]));
+	        	final String idSession = RequestContextHolder.currentRequestAttributes().getSessionId();
+	        	
+	        	return ConnectedDTO.builder()
+	    				.username(values[0])
+	    				.grantedAuthorities(token.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+	    				.idSession(idSession)
+	    				.build();
+	        } else {
+	        	throw new InvalidRequestException("Petición de autorización incorrecto");
+	        }
+	    }
+	    else {
+	    	throw new InvalidRequestException("Se debe incluir la autenticación en la petición.");
+	    }
+	}
+	
+	@GetMapping("/connection") 
+	public ResponseEntity<ConnectedDTO> connected(Authentication auth) {
+		
+		final Boolean isConected = Optional.ofNullable(auth).map(Authentication::getPrincipal).isPresent();
+		final HttpStatus statusConnection = isConected ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
+		
+		return new ResponseEntity<>(statusConnection);
+	}
+}
--- src/main/java/com/fot/backcontask/controller/UserController.java
+++ src/main/java/com/fot/backcontask/controller/UserController.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,9 +39,6 @@ public class UserController {
 	@Autowired
 	UserMapper userMapper;
 	
-	@Autowired
-	PasswordEncoder passwordEncoder;
-	
 	@GetMapping
 	@ApiOperation(notes="Devuelve una lista de usuarios paginado, cada página tendrá un tamaño máximo de 10", tags= { "User" }, value="All user")
 	@ApiResponses({ @ApiResponse(code = 200, response= UserDTO.class, message="All users"),
@@ -65,7 +61,6 @@ public class UserController {
 		if(dto.getIdUser() != null) 
 			throw new InvalidRequestException("El idUser no se puede recibir en el body");
 		final User user = userMapper.dtoToModel(dto);
-		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		final User createUser = userService.create(user);
 		return userMapper.modelToDto(createUser);
 	}
@@ -76,7 +71,6 @@ public class UserController {
 			throw new InvalidRequestException("El idUser no se puede recibir en el body");
 		final User user = userService.getAndCheck(id);
 		userService.setValues(user, userMapper.dtoToModel(dto));
-		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		userService.update(user);
 	}
 	
